4941657b91353641b1ed1c9bf8bfb2936a013c65
Ron Bronson <ron@ronbronson.com>
2021-01-02 11:51:00 -0800

    Update Journal.vue

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="journal-hero">
         <h1 class="journal-header">
-          a wise person once said...
+          Latest Notes
         </h1>
       </div>
     </div>
